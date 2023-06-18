01ae726158888b1517626fa037936ccf3435bc43
BluedyAishela_ <94249947+BluedyRimuru@users.noreply.github.com>
2023-06-18 15:51:12 +0200

    Init projet & version 1.8.8 spigot

---
+++ src/main/java/me/bluedyaishela/silverenchanter/SilverEnchanter.java
@@ -0,0 +1,17 @@
+package me.bluedyaishela.silverenchanter;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SilverEnchanter extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
